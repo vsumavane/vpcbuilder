75d116fcc98dd7c8809314c9a649d25d895d4cf2
Vishal Umavane <vsumavane@outlook.com>
2024-03-10 20:27:38 +0530

    site files

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # PCbuild_Website
 
+Tasks :
+Home Page - Login / Sign up Button
+            - Build | Analyse
+
+Login Page - 
+
+Sign up Page - 
+
+Build page - Partpicker, Build Space
+
+PartPicker - components section : CPU (main)| others (Monitor, Keyboard, Mouse, Speakers, Headphones, Microphone, FaceCam)
+
+
+
+
 Presentation file : [https://bit.ly/pcbuild_ppt]
---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+function openLoginPopup() {
+    document.getElementById('loginPopup').style.display = 'block';
+}
+
+function closeLoginPopup() {
+    document.getElementById('loginPopup').style.display = 'none';
+}
+
+function openSignupPopup() {
+    document.getElementById('signupPopup').style.display = 'block';
+}
+
+function closeSignupPopup() {
+    document.getElementById('signupPopup').style.display = 'none';
+}
