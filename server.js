a57caddfdb4b8715ce6dfaa520fefc689a55e0d2
Vishal Umavane <vsumavane@outlook.com>
2024-03-12 16:48:45 +0530

    rename to components

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 3000;
 // Serve static files (HTML, CSS, images, etc.) from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route for the PC components page
+app.get('/components', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'components.html'));
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
